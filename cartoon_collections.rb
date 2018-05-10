f002f8fb4d50d37ebc462018571b881bd321bea5
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 00:57:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves(names)# code an argument here
-  index = 1# Your code here
-  names = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  names.each_with_index{|name, index|;}
+def roll_call_dwarves(dwarves)# code an argument here
+  # Your code here
+  # names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarves.each_with_index do |name, index|
   puts "#{index} #{name}"
-
+  end
 end
 
 def summon_captain_planet# code an argument here
