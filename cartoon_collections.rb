b7df28c07cf14325d73c3d01aa0c15f425d3e6a6
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 01:08:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map! do |call|
   call.capitalize } + "!"
 end
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
