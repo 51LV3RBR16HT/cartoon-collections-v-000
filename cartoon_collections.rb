2ac1f992524d01fc70e97c6770cf6a0993fde1f2
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 00:42:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   # dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarves.each_with_index{|name, index|;}
+  dwarves.each_with_index{|name, index|}
     puts "#{index} #{name}"
 
 end
