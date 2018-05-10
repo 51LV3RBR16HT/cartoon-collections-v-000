c2b89baa6320fe23b1df55f555d9cd5d0edd2cdb
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 01:04:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet(calls)# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
   
